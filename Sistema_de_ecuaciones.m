a49b20edeed746c874ec96fba8f0abe2575a4d7f
Willinton De Aquino <51212952+WDeAquino@users.noreply.github.com>
2021-02-08 18:06:00 -0600

    Solucion de sistemas de escuaciones de primer orden

---
+++ Sistema_de_ecuaciones.m
@@ -0,0 +1,7 @@
+%%Autor: Willinton De Aquino Perez
+
+%%Definimos nuestro sistema de ecuaciónes de la forma Ax=b siendo A una matriz y b el vector de resultados
+%% x es el vector de solución
+A = [1 2 3;3 2 1;5 9 8]
+b = [9 15 3]'
+x = A\b
